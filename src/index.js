d1a2de08ff815a1d58fda88741c109ea3b4062c2
Hemil-Hansora <hemilhansora1729@gmail.com>
2025-01-25 02:12:31 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import 'dotenv/config'
+import "dotenv/config";
 
 import connectDB from "./db/index.js";
-
-
+import { app } from "./app.js";
 
 connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.error("Error :", err);
+            throw err;
+        });
+
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MondoDb connection error !! : ", err);
+    });
